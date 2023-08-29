fb3e60cbb451d6e0c1ffe4eec84b8083d1f97a6f
Guilherme Swerts <p.guilherme.swerts@viveo.com.br>
2023-08-29 19:57:52 -0300

    novo style

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const scroolToForm = (e) => {
-    document.getElementById("image_form").scrollIntoView({
+    document.getElementById("form").scrollIntoView({
         behavior: "smooth",
         block: "start"
     });
 }
 
-const validForm = (name, email, msg) => { return name !== '' && email !== '' && msg !== ''; }
+const validForm = (name, email, msg, phone) => { return name !== '' && email !== '' && msg !== '' && phone !== ''; }
 
 document.querySelector("form")
     .addEventListener("submit", onSubmit);
@@ -15,18 +15,21 @@ function onSubmit(e) {
     const name = document.querySelector('input[name="name"]');
     const email = document.querySelector('input[name="email"]');
     const msg = document.querySelector('textarea[name="comment"]');
-    if (validForm(name.value, email.value, msg.value)) {
-        let send = enviarEmail(email.value, name.value, msg.value);
+    const phone = document.querySelector('input[name="phone"]');
+    debugger
+    if (validForm(name.value, email.value, msg.value, phone.value)) {
+        let send = enviarEmail(email.value, name.value, msg.value, phone.value);
         alert(send ? 'Email enviado com sucesso!\n Em 24 horas vamos entrar em contato!' : 'Ops...Algo deu erro ao enviar o email.')
     }
 }
 
-async function enviarEmail(to, name, message) {
+async function enviarEmail(to, name, message, phone) {
     let param = {
         name: name,
         to: 'gsctech@hotmail.com',
         replyto: to,
-        message: message
+        message: message,
+        phone: phone
     };
 
     const serviceId = 'service_jd43wir';
